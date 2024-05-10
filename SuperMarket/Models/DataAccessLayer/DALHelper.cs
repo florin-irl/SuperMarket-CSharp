bd8f23f4a111ecc1c3aa4e63facec4ca99b0b0cc
florin-irl <ivana.florin1987@gmail.com>
2024-05-10 10:36:22 +0300

    Create DALHelper

---
+++ SuperMarket/Models/DataAccessLayer/DALHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Models.DataAccessLayer
+{
+    public static class DALHelper
+    {
+        private static readonly string _connectionString = "Data Source=DESKTOP-IFEPT2A;Initial Catalog=dbHypermarket;Integrated Security=SSPI";
+
+
+        public static SqlConnection Connection
+        {
+            get
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                return connection;
+            }
+        }
+    }
+}

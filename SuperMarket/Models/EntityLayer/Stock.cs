dc9c01758236948915fadb08935c19a6a2f3b472
florin-irl <ivana.florin1987@gmail.com>
2024-05-10 10:31:30 +0300

    Create Stock Class

---
+++ SuperMarket/Models/EntityLayer/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Models.EntityLayer
+{
+    public class Stock
+    {
+        public int StockId { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public DateTime SupplyDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int ProductId { get; set; }
+        public decimal AquisitionCost { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}

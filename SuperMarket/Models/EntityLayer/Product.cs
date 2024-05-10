d6fe524ecdcab85be98da9efdc594436bae0b8fd
florin-irl <ivana.florin1987@gmail.com>
2024-05-10 10:18:18 +0300

    Create Category and Product Class

---
+++ SuperMarket/Models/EntityLayer/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Models.EntityLayer
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public int CategoryId { get; set; }
+        public int ProducerId { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}

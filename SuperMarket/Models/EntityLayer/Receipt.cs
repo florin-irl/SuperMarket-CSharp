aa59a4b148d489f32a39efa96d64047a3c684a84
florin-irl <ivana.florin1987@gmail.com>
2024-05-10 10:25:30 +0300

    Create Receipt Class

---
+++ SuperMarket/Models/EntityLayer/Receipt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Models.EntityLayer
+{
+    public class Receipt
+    {
+        public int ReceiptID { get; set; }
+        public DateTime IssueDate { get; set; }
+        public int UserID { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

0c93e7bb930e4cd929667ce00d23e1806722525b
florin-irl <ivana.florin1987@gmail.com>
2024-05-10 10:20:13 +0300

    Create Offer Class

---
+++ SuperMarket/Models/EntityLayer/Offer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Models.EntityLayer
+{
+    public class Offer
+    {
+        public int OfferId { get; set; }
+        public int ProductId { get; set; }
+        public int DiscountPercentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}

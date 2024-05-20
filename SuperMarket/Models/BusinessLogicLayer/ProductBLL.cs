32a5a3202c4e8aba10185ee9839c77892d298809
florin-irl <ivana.florin1987@gmail.com>
2024-05-20 21:48:01 +0300

    Implemented the Search function for Cashier

--- SuperMarket/Models/BusinessLogicLayer/ProductBLL.cs
+++ SuperMarket/Models/BusinessLogicLayer/ProductBLL.cs
@@ -1,6 +1,7 @@
 ﻿using SuperMarket.Models.DataAccessLayer;
 using SuperMarket.Models.EntityLayer;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -12,6 +13,9 @@ namespace SuperMarket.Models.BusinessLogicLayer
     public class ProductBLL
     {
         private ProductDAL _productDAL = new ProductDAL();
+        private CategoryDAL _categoryDAL = new CategoryDAL();
+        private ProducerDAL _producerDAL = new ProducerDAL();
+        private StockDAL _stockDAL = new StockDAL();
 
         public ObservableCollection<Product> GetAllProducts()
         {
@@ -31,6 +35,99 @@ namespace SuperMarket.Models.BusinessLogicLayer
         public void DeleteProduct(Product product)
         {
             _productDAL.DeleteProduct(product);
+        }
+
+        public ObservableCollection<Product> GetProductsByName(string name)
+        {
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            ObservableCollection<Product> allProducts = GetAllProducts();
+            ObservableCollection<Stock>  stocks = _stockDAL.GetAllStocks();
+            foreach (Product product in allProducts)
+            {
+                   if (product.Name.Contains(name) && stocks.Any(s => s.ProductId == product.ProductId))
+                {
+                    products.Add(product);
+                }
+            }
+            return products;
+        }
+
+        public ObservableCollection<Product> GetProductsByBarcode(string barcode)
+        {
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            ObservableCollection<Product> allProducts = GetAllProducts();
+            ObservableCollection<Stock> stocks = _stockDAL.GetAllStocks();
+            foreach (Product product in allProducts)
+            {
+                if (product.Barcode.Contains(barcode) && stocks.Any(s => s.ProductId == product.ProductId))
+                {
+                    products.Add(product);
+                }
+            }
+            return products;
+        }
+
+        public ObservableCollection<Product> GetProductsByCategory(string category)
+        {
+            Category selectedCategory;
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            ObservableCollection<Product> allProducts = GetAllProducts();
+            ObservableCollection<Stock> stocks = _stockDAL.GetAllStocks();
+            foreach (Product product in allProducts)
+            {
+                selectedCategory = _categoryDAL.GetCategoryById(product.CategoryId);
+                if (selectedCategory.Name == category && stocks.Any(s => s.ProductId == product.ProductId))
+                {
+                    products.Add(product);
+                }
+            }
+            return products;
+        }
+
+        public ObservableCollection<Product> GetProductsByProducer(string producer)
+        {
+            Producer selectedProducer;
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            ObservableCollection<Product> allProducts = GetAllProducts();
+            ObservableCollection<Stock> stocks = _stockDAL.GetAllStocks();
+            foreach (Product product in allProducts)
+            {
+                selectedProducer = _producerDAL.GetProducerById(product.ProducerId);
+                if (selectedProducer.Name == producer && stocks.Any(s => s.ProductId == product.ProductId))
+                {
+                    products.Add(product);
+                }
+            }
+            return products;
+        }
+
+        public ObservableCollection<Product> GetProductsByExpirationDate(string expirationDate)
+        {
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            ObservableCollection<Product> allProducts = GetAllProducts();
+            ObservableCollection<Stock> stocks = _stockDAL.GetAllStocks();
+            foreach (Stock stock in stocks)
+            {
+                Console.WriteLine(stock.ExpirationDate);
+            }
+            string[] dateParts = expirationDate.Split('/');
+            int day = int.Parse(dateParts[0]);
+            int month = int.Parse(dateParts[1]);
+            int year = int.Parse(dateParts[2]);
+            DateTime date = new DateTime(year, month, day);
+            Console.Write(date);
+
+
+            foreach (Product product in allProducts)
+            {
+                bool hasMatchingStock = stocks.Any(s => s.ProductId == product.ProductId && s.ExpirationDate == date);
+               
+                if (hasMatchingStock)
+                {
+                    products.Add(product);
+                }
+            }
+            return products;
         }
     }
 }
---
+++ SuperMarket/ViewModels/MainCashierPageViewModel.cs
@@ -0,0 +1,91 @@
+﻿using SuperMarket.Models.BusinessLogicLayer;
+using SuperMarket.Models.EntityLayer;
+using SuperMarket.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SuperMarket.ViewModels
+{
+    public class MainCashierPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<String> FilterList { get; set; }
+
+        private ObservableCollection<Product> _selectedProducts;
+        public ObservableCollection<Product> SelectedProducts
+        {
+            get { return _selectedProducts; }
+            set
+            {
+                _selectedProducts = value;
+                OnPropertyChanged("SelectedProducts");
+            }
+        }
+
+        public ObservableCollection<Product> CartList { get; set; }
+        
+        public string SearchText { get; set; }
+
+        public string SelectedFilter { get; set; }
+
+        public Product SelectedProduct { get; set; }
+
+        private ProductBLL _productBLL = new ProductBLL();
+
+        public ICommand SearchCommand { get; set; }
+        public MainCashierPageViewModel()
+        {
+            FilterList = new ObservableCollection<string>
+            {
+                "Name",
+                "Barcode",
+                "Category",
+                "Producer",
+                "Expiration Date"
+
+            };
+
+            SelectedProducts = new ObservableCollection<Product>();
+            CartList = new ObservableCollection<Product>();
+            SearchCommand = new RelayCommand<string>(Search);
+        }
+
+        private void Search(string filter)
+        {
+            if (string.IsNullOrEmpty(SearchText))
+            {
+                SelectedProducts.Clear();
+                return;
+            }
+
+
+            switch (SelectedFilter)
+            {
+                case "Name":
+                    SelectedProducts = _productBLL.GetProductsByName(SearchText);
+                    break;
+                case "Barcode":
+                    SelectedProducts = _productBLL.GetProductsByBarcode(SearchText);
+                    break;
+                case "Category":
+                    SelectedProducts = _productBLL.GetProductsByCategory(SearchText);
+                    break;
+                case "Producer":
+                    SelectedProducts = _productBLL.GetProductsByProducer(SearchText);
+                    break;
+                case "Expiration Date":
+                    SelectedProducts = _productBLL.GetProductsByExpirationDate(SearchText);
+                    break;
+            }
+            foreach(Product product in SelectedProducts)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine("IM HERE");
+            }
+        }
+    }
+}
